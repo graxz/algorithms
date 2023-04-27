4137c11ac8bb0ad7e1397f3d8f443cda8546828c
Isaddora Freitas <ACT081@einstein.br>
2023-04-27 18:13:57 +0000

    feat: added stack class

---
+++ stacks/index.js
@@ -0,0 +1,32 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if (this.items.length == 0) return [];
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+}
+
+module.exports = Stack;
