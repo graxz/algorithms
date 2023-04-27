3899897e54feaa11aeda61b8ff4cabd3f44f1943
Isaddora Freitas <ACT081@einstein.br>
2023-04-27 20:00:08 +0000

    test: refactor name of methods + refactor logic of methods

--- linked-list/test/index.test.js
+++ linked-list/test/index.test.js
@@ -3,50 +3,28 @@ const LinkedList = require("../index");
 
 describe('LinkedList', () => {
 
-    // Tests that adding a single element to the list works correctly. 
-    it("test_add_single_element", () => {
+    
+    // Tests that a value can be appended to an empty list. 
+    it("test_append_empty_list", () => {
         const list = new LinkedList();
-        list.add(1);
-        expect(list.toString()).toBe("1");
+        list.append(1);
+        expect(list.head.value).toBe(1);
     });
 
-    // Tests that adding multiple elements to the list works correctly. 
-    it("test_add_multiple_elements", () => {
+    // Tests that a value can be appended to a non-empty list. 
+    it("test_append_non_empty_list", () => {
         const list = new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        expect(list.toString()).toBe("1 2 3");
+        list.append(1);
+        list.append(2);
+        expect(list.head.value).toBe(1);
+        expect(list.head.next.value).toBe(2);
     });
 
-    // Tests that removing the head element from the list works correctly. 
-    it("test_remove_head_element", () => {
+    // Tests that the search method returns null when the value is not found in the list. 
+    it("test_search_not_found", () => {
         const list = new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.remove(1);
-        expect(list.toString()).toBe("2");
-    });
-
-    // Tests that checking if a null value is in the list works correctly. 
-    it("test_contains_null_value", () => {
-        const list = new LinkedList();
-        list.add(null);
-        expect(list.contains(null)).toBe(true);
-    });
-
-    // Tests that adding duplicate elements to the list works correctly. 
-    it("test_add_duplicate_elements", () => {
-        const list = new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        expect(list.toString()).toBe("1 2 1");
-    });
-
-    // Tests that converting an empty list to a string works correctly. 
-    it("test_to_string_empty_list", () => {
-        const list = new LinkedList();
-        expect(list.toString()).toBe("");
+        list.append(1);
+        list.append(2);
+        expect(list.search(3)).toBeNull();
     });
 })
