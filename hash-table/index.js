df47c25b87d729a13a5a7965bdf84d67a8f6d384
Isaddora Freitas <ACT081@einstein.br>
2023-04-28 15:54:58 +0000

    feat: added hash table

---
+++ hash-table/index.js
@@ -0,0 +1,45 @@
+class HashTable {
+    constructor() {
+        this.table = new Array(137);
+    }
+    
+    simpleHash(data) {
+        let total = 0;
+        for (let i = 0; i < data.length; i++) {
+        total += data.charCodeAt(i);
+        }
+        return total % this.table.length;
+    }
+    
+    betterHash(data) {
+        const H = 37;
+        let total = 0;
+        for (let i = 0; i < data.length; i++) {
+        total += H * total + data.charCodeAt(i);
+        }
+        total = total % this.table.length;
+        if (total < 0) {
+        total += this.table.length - 1;
+        }
+        return parseInt(total);
+    }
+
+    put(data) {
+        let pos = this.betterHash(data);
+        this.table[pos] = data;
+    }
+
+    get(key) {
+        return this.table[this.betterHash(key)];
+    }
+
+    remove(key) {
+        this.table[this.betterHash(key)] = undefined;
+    }
+
+    contains(key) {
+        return this.table[this.betterHash(key)] !== undefined;
+    }
+}
+
+module.exports = HashTable;
