5e860686b8dcec30d66a5dc42f020fe4e55cfd3f
Isaddora Freitas <ACT081@einstein.br>
2023-05-03 14:45:59 +0000

    test: refactor circular linked list

--- circular-linked-list/test/index.test.js
+++ circular-linked-list/test/index.test.js
@@ -2,10 +2,7 @@ const { describe } = require("node:test");
 const CircularLinkedList = require("../index");
 
 describe('Circular Linked List', () => {
-    
-    
-    // Tests that append method adds a new node to the end of the list with valid input. 
-    it("test_append_valid_input", () => {
+    it("Should adds a new node to the end of the list with valid input", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -14,8 +11,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that prepend method adds a new node to the beginning of an empty list. 
-    it("test_prepend_empty_list", () => {
+    it("Should adds a new node to the beginning of an empty list", () => {
         const cll = new CircularLinkedList();
         cll.prepend(1);
         expect(cll.head.value).toBe(1);
@@ -23,8 +19,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that insert method adds a new node to the beginning of the list when index is 0. 
-    it("test_insert_index_0", () => {
+    it("Should adds a new node to the beginning of the list when index is 0", () => {
         const cll = new CircularLinkedList();
         cll.insert(1, 0);
         expect(cll.head.value).toBe(1);
@@ -32,8 +27,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that remove method removes the head node correctly. 
-    it("test_remove_head_node", () => {
+    it("Should remove method removes the head node correctly", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -43,16 +37,14 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that search method returns null when searching for a non-existent value. 
-    it("test_search_non_existent_value", () => {
+    it("Should returns null when searching for a non-existent value", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
         expect(cll.search(3)).toBe(null);
     });
 
-    // Tests inserts a value at a specific index. 
-    it("test_insert", () => {
+    it("Should inserts a value at a specific index", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -64,8 +56,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests removes a value from the list. 
-    it("test_remove", () => {
+    it("Should removes a value from the list", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -77,8 +68,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that tail.next points to head. 
-    it("test_circular_behavior", () => {
+    it("Should tail.next points to head", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -86,8 +76,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests searches for a value in the list. 
-    it("test_search", () => {
+    it("Should searches for a value in the list", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -96,16 +85,14 @@ describe('Circular Linked List', () => {
         expect(cll.search(4)).toBe(null);
     });
 
-    // Tests that the remove method correctly handles removing a value from an empty list. 
-    it("test_remove_from_empty_list", () => {
+    it("Should remove method correctly handles removing a value from an empty list", () => {
         const cll = new CircularLinkedList();
         cll.remove(1);
         expect(cll.head).toBeNull();
         expect(cll.tail).toBeNull();
     });
 
-    // Tests that the traverseToIndex method correctly traverses the list.
-    it("test_traverse_to_index", () => {
+    it("Should traverseToIndex method correctly traverses the list", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -113,14 +100,12 @@ describe('Circular Linked List', () => {
         expect(cll.traverseToIndex(1).value).toBe(2);
     });
 
-    // Tests that the search method correctly handles searching an empty list.
-    it("test_search_empty_list", () => {
+    it("Should search method correctly handles searching an empty list", () => {
         const cll = new CircularLinkedList();
         expect(cll.search(1)).toBe(null);
     });
 
-    // Tests that the remove method correctly handles reassigning the tail when removing the last node.
-    it("test_remove_tail_node", () => {
+    it("Should remove method correctly handles reassigning the tail when removing the last node", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -129,8 +114,7 @@ describe('Circular Linked List', () => {
         expect(cll.tail.next).toBe(cll.head);
     });
 
-    // Tests that the remove method correctly handles reassigning the next property of the previous node when removing a node.
-    it("test_remove_reassign_next_property", () => {
+    it("Should remove method correctly handles reassigning the next property of the previous node when removing a node", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -139,8 +123,7 @@ describe('Circular Linked List', () => {
         expect(cll.head.next.value).toBe(3);
     });
 
-    // Tests that the remove method correctly handles reassigning the next property of the previous node when removing the tail node.
-    it("test_remove_tail_reassign_next_property", () => {
+    it("Should remove method correctly handles reassigning the next property of the previous node when removing the tail node", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -150,8 +133,7 @@ describe('Circular Linked List', () => {
         expect(cll.head.next.value).toBe(2);
     });
 
-    // Tests that the remove method correctly handles reassigning the next property of the previous node when removing the head node.
-    it("test_remove_head_reassign_next_property", () => {
+    it("Should remove method correctly handles reassigning the next property of the previous node when removing the head node", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -160,8 +142,7 @@ describe('Circular Linked List', () => {
         expect(cll.head.next.value).toBe(3);
     });
 
-    // Tests that the remove method correctly handles assigning the head with the next node when removing the head node.
-    it("test_remove_head_reassign_head", () => {
+    it("Should remove method correctly handles assigning the head with the next node when removing the head node", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
@@ -170,8 +151,7 @@ describe('Circular Linked List', () => {
         expect(cll.head.value).toBe(2);
     });
 
-    // Tests that the prepend method correctly handles when reassigning the tail next property with the new head.
-    it("test_prepend_reassign_tail_next_property", () => {
+    it("Should prepend method correctly handles when reassigning the tail next property with the new head", () => {
         const cll = new CircularLinkedList();
         cll.append(1);
         cll.append(2);
